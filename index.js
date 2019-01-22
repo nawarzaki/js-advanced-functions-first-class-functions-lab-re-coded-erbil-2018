bcbdbff8f86b2f7b8e52ad60cf9c756c3870c96f
nawaar.zakee <nawaar.zakee@yahoo.com>
2019-01-22 15:03:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -37,6 +37,6 @@ const fareTripler = function (createFareMultiplier) {
 
 }
 function selectDifferentDrivers(drivers, m) {
-m = returnFirstTwoDrivers(drivers) && returnLastTwoDrivers(drivers)
+m = returnFirstTwoDrivers(drivers) || returnLastTwoDrivers(drivers)
 return m
 }
