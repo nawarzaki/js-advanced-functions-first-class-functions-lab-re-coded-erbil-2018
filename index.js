f48dda07980a5eeae1903f211c3cb81f81b2fae8
nawaar.zakee <nawaar.zakee@yahoo.com>
2019-01-22 14:35:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+ function returnFirstTwoDrivers(drivers) {
+ const x = function (drivers) {
+   return drivers.slice(0,2)
+ }
+ return x(drivers)
+ }
+
+ function returnLastTwoDrivers(drivers) {
+  const x = function (drivers) {
+    return drivers.slice(2,4)
+  }
+  return x(drivers)
+  }
+
+function selectingDrivers(drivers) {
+  let x = []
+  x.push(returnFirstTwoDrivers(drivers), returnLastTwoDrivers(drivers))
+  return x
+}
