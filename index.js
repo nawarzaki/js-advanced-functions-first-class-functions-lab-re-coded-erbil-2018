3e3af8f8a05fe0789f5823912817745fb60d26c3
nawaar.zakee <nawaar.zakee@yahoo.com>
2019-01-22 14:36:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,15 +7,3 @@ d(drivers)
  return x(drivers)
  }
 
- function returnLastTwoDrivers(drivers) {
-  const x = function (drivers) {
-    return drivers.slice(2,4)
-  }
-  return x(drivers)
-  }
-
-function selectingDrivers(drivers) {
-  let x = []
-  x.push(returnFirstTwoDrivers(drivers), returnLastTwoDrivers(drivers))
-  return x
-}
